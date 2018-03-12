b334d12f23b3973ba5aa0b6360a382fdbc121789
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-03-13 00:25:38 +0700

    :lipstick: Minimize UI

--- src/demo/DemoRouteComponent.js
+++ src/demo/DemoRouteComponent.js
@@ -4,8 +4,7 @@ import styled from 'styled-components'
 const DemoRouteComponent = (props) => {
   return (
     <h2>
-      Demo Route Component <br />
-      (param = <span className="badge badge-primary">{ props.match.params.number }</span>)
+      param = <span className="badge badge-primary">{ props.match.params.number }</span>
     </h2>
   )
 }
